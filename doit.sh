42f46ec74b2a9288e3fb2c3b8550bd4be3266cc6
Eric Coleman <eric@epiccoleman.com>
2018-01-14 09:19:52 -0500

    update gitignore

--- doit.sh
+++ doit.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#date +%Y-%m-%d-%h-%M!/bin/bash
 
-cp -r /mnt/c/Users/Eric/Minecraft/server/world . 
+cp -r /mnt/c/Users/Eric/Minecraft/server/world .
 
 ./overviewer.py --config=config.txt
 
 cp -r map /mnt/c/Users/Eric/Minecraft/
+
+mkdir -p /mnt/d/minecraft-backups/$(date +%Y-%m-%d-%H-%M)-spacegrass
+cp -r world /mnt/d/minecraft-backups/$(date +%Y-%m-%d-%H-%M)-spacegrass &
